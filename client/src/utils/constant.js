7e2f20c32de5a68099240b6c8364a72d0ef7ec10
dhruvpatel374 <157447916+dhruvpatel374@users.noreply.github.com>
2024-10-20 17:10:21 +0530

    Update constant.js

--- client/src/utils/constant.js
+++ client/src/utils/constant.js
@@ -4,8 +4,8 @@ export const CDN_URL =
 export const SWIGGY_API = "https://zoggy-server.vercel.app/api/res";
 // ("http://localhost:3001/api/res");
 export const SWIGGY_API_MOBILE =
-  // "https://zoggy-server.vercel.app/api/res";
-  "http://localhost:3001/api/mobileres";
+  "https://zoggy-server.vercel.app/api/mobileres";
+  // "http://localhost:3001/api/mobileres";
 export const MENU_API = "https://zoggy-server.vercel.app/api/menu";
 // "http://localhost:3001/api/menu";
 export const City = [
